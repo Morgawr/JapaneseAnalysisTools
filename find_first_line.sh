00218d74432d8362091479e7385c6c99f429858b
Federico 'Morg' Pareschi <morgawr@gmail.com>
2021-03-21 23:37:48 +0900

    Integrated line extracting script with heatmap script

--- find_first_line.sh
+++ find_first_line.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-WORDS=$1
+WORD=$1
 FILE=$2
 
-for WORD in $(cat $WORDS) ; do
-  FOUND=$(grep -n -m 1 $WORD $FILE)
-  if [[ ! -z "$FOUND" ]] ; then
-    FIRST=$(echo $FOUND | cut -d':' -f1)
-    FOUND=$(echo $FOUND | sed "s/$FIRST\://")
-    echo "$FIRST:$WORD:$(echo $FOUND | xargs | sed 's/　//g')"
-  fi
-done
+REGEX="${WORD::-1}$(./print_verb_conjugations.sh)" # Use this for verbs
+#REGEX="$(./print_hiragana_hash.sh)$WORD$(./print_hiragana_hash.sh)" # use this for single-kanji words
+#REGEX="$WORD" # use this for normal words
+FOUND=$(grep -n -m 1 $WORD $FILE)
+
+if [[ ! -z "$FOUND" ]] ; then
+  #LINES=$(echo $(grep -En "$REGEX" $FILE | cut -d':' -f1) | tr ' ' ',')
+  grep -En --color "$REGEX" $FILE
+  #echo "$LINES"
+fi
--- generate_heatmap.sh
+++ generate_heatmap.sh
@@ -2,16 +2,8 @@
 
 WORDS=$1
 FILE=$2
-LINECOUNT=$(wc -l $FILE | cut -d' ' -f1)
 
 for WORD in $(cat $WORDS) ; do
-  #WORD="黙る"
-  #REGEX="${WORD::-1}$(./print_verb_conjugations.sh)" # Use this for verbs
-  #REGEX="$(./print_hiragana_hash.sh)$WORD$(./print_hiragana_hash.sh)" # use this for single-kanji words
-  REGEX="$WORD" # use this for normal words
-  FOUND=$(grep -En -m 1 "$REGEX" $FILE)
-  if [[ ! -z "$FOUND" ]] ; then
-    LINES=$(echo $(grep -En "$REGEX" $FILE | cut -d':' -f1) | tr ' ' ',')
-    echo "$LINES:$WORD"
-  fi
+  LINES=$(echo $(./find_first_line.sh $WORD $FILE | cut -d':' -f1) | tr ' ' ',')
+  echo "$LINES:$WORD"
 done
