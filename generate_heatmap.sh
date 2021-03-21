ab2c72c334773ea037fb1fcf48e7f5e5f84d5921
Federico 'Morg' Pareschi <morgawr@gmail.com>
2021-03-21 23:16:14 +0900

    Refactored common regex for word types

--- generate_heatmap.sh
+++ generate_heatmap.sh
@@ -5,12 +5,13 @@ FILE=$2
 LINECOUNT=$(wc -l $FILE | cut -d' ' -f1)
 
 for WORD in $(cat $WORDS) ; do
-#WORD="終わ"
-  FOUND=$(grep -n -m 1 $WORD $FILE)
+  #WORD="黙る"
+  #REGEX="${WORD::-1}$(./print_verb_conjugations.sh)" # Use this for verbs
+  #REGEX="$(./print_hiragana_hash.sh)$WORD$(./print_hiragana_hash.sh)" # use this for single-kanji words
+  REGEX="$WORD" # use this for normal words
+  FOUND=$(grep -En -m 1 "$REGEX" $FILE)
   if [[ ! -z "$FOUND" ]] ; then
-    #LINES=$(echo $(grep -En "$(./print_hiragana_hash.sh)$WORD$(./print_hiragana_hash.sh)" $FILE | cut -d':' -f1) | tr ' ' ',') # use this for single-kanji words
-    #LINES=$(echo $(grep -En "${WORD::-1}$(./print_verb_conjugations.sh)" $FILE | cut -d':' -f1) | tr ' ' ',') # use this for verb words
-    LINES=$(echo $(grep -n $WORD $FILE | cut -d':' -f1) | tr ' ' ',') # use this for normal words
+    LINES=$(echo $(grep -En "$REGEX" $FILE | cut -d':' -f1) | tr ' ' ',')
     echo "$LINES:$WORD"
   fi
 done
