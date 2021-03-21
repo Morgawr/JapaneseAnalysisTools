30f50cc30c8274806111829aa97913c62f32f51b
Federico 'Morg' Pareschi <morgawr@gmail.com>
2021-03-21 23:38:35 +0900

    Missed to fix FOUND boolean

--- find_first_line.sh
+++ find_first_line.sh
@@ -6,10 +6,8 @@ FILE=$2
 REGEX="${WORD::-1}$(./print_verb_conjugations.sh)" # Use this for verbs
 #REGEX="$(./print_hiragana_hash.sh)$WORD$(./print_hiragana_hash.sh)" # use this for single-kanji words
 #REGEX="$WORD" # use this for normal words
-FOUND=$(grep -n -m 1 $WORD $FILE)
+FOUND=$(grep -En -m 1 "$REGEX" $FILE)
 
 if [[ ! -z "$FOUND" ]] ; then
-  #LINES=$(echo $(grep -En "$REGEX" $FILE | cut -d':' -f1) | tr ' ' ',')
   grep -En --color "$REGEX" $FILE
-  #echo "$LINES"
 fi
