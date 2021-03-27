22257acb0af66cddbe615a5b09a376b4f7296926
Federico 'Morg' Pareschi <morgawr@gmail.com>
2021-03-27 09:57:56 +0900

    Fixed some conjugation/sorting of args

--- find_first_line.sh
+++ find_first_line.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-WORD=$1
-FILE=$2
+WORD=$2
+FILE=$1
 
 REGEX="${WORD::-1}$(./print_verb_conjugations.sh)" # Use this for verbs
 #REGEX="$(./print_hiragana_hash.sh)$WORD$(./print_hiragana_hash.sh)" # use this for single-kanji words
--- generate_heatmap.sh
+++ generate_heatmap.sh
@@ -4,6 +4,6 @@ WORDS=$1
 FILE=$2
 
 for WORD in $(cat $WORDS) ; do
-  LINES=$(echo $(./find_first_line.sh $WORD $FILE | cut -d':' -f1) | tr ' ' ',')
+  LINES=$(echo $(./find_first_line.sh $FILE $WORD | cut -d':' -f1) | tr ' ' ',')
   echo "$LINES:$WORD"
 done
