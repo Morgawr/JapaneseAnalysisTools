c81cedbfcc00e7381de4f7da0b1dc0907f98e581
Federico 'Morg' Pareschi <morgawr@gmail.com>
2021-03-27 10:01:49 +0900

    Fixed some missing conjugations and verb regex

--- find_first_line.sh
+++ find_first_line.sh
@@ -3,7 +3,7 @@
 WORD=$2
 FILE=$1
 
-REGEX="${WORD::-1}$(./print_verb_conjugations.sh)" # Use this for verbs
+REGEX="$(./print_hiragana_hash.sh)${WORD::-1}$(./print_verb_conjugations.sh)" # Use this for verbs
 #REGEX="$(./print_hiragana_hash.sh)$WORD$(./print_hiragana_hash.sh)" # use this for single-kanji words
 #REGEX="$WORD" # use this for normal words
 FOUND=$(grep -En -m 1 "$REGEX" $FILE)
